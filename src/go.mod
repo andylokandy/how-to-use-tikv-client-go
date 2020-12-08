d5d6d8f3d5833ed4711eb1839a687be8a41049ed
Andy Lok <andylokandy@hotmail.com>
2020-12-08 23:58:16 +0800

    Initial commit

---
+++ src/go.mod
@@ -0,0 +1,7 @@
+module github.com/andylokandy/goclient2
+
+go 1.14
+
+replace github.com/pingcap/tidb => ../tidb
+
+require github.com/pingcap/dm v1.1.0-alpha.0.20200521025928-83063141c5fd // indirect
---
+++ src/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pingcap/tidb/store/tikv"
+)
+
+func main() {
+	run()
+}
+
+func run() error {
+	driver := tikv.Driver{}
+	store, err := driver.Open("tikv://localhost:2379?cluster=1")
+	if err != nil {
+		return err
+	}
+
+	txn, err := store.Begin()
+	if err != nil {
+		return err
+	}
+
+	txn.Set([]byte("k1"), []byte("v1"))
+	txn.Commit(context.Background())
+
+	txn2, err := store.Begin()
+	if err != nil {
+		return err
+	}
+
+	val, err := txn2.Get(context.Background(), []byte("k1"))
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("k1:\t %v\n", val)
+
+	return nil
+}
